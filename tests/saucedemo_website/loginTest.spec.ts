109ce6b4d305c96b7fc7b31c863be6962914d261
vraj-verma <vermavraj77@gmail.com>
2022-06-30 00:04:40 +0530

    update with new test case

--- tests/saucedemo_website/loginTest.spec.ts
+++ tests/saucedemo_website/loginTest.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Login Test', () => {
+test.describe('Title', () => {
     test('Should match the title', async ({ browser }) => {
         const fresh_browser = await browser.newContext();
         const newPage = await fresh_browser.newPage();
@@ -9,5 +9,44 @@ test.describe('Login Test', () => {
         await newPage.waitForTimeout(7000);
         // const title = await newPage.title();
         await expect(newPage).toHaveTitle('Swag Labs');
-    })
-})
+    });
+});
+
+const credentials = [
+    {
+        user_name: 'standard_user',
+        pass_word: 'secret_sauce'
+    },
+    {
+        user_name: 'locked_out_user',
+        pass_word: 'secret_sauce'
+    },
+    {
+        user_name: 'problem_user',
+        pass_word: 'secret_sauce'
+    },
+    {
+        user_name: 'performance_glitch_user',
+        pass_word: 'secret_sauce'
+    },
+];
+
+test.describe('Login', () => {
+    test.only('Login should happen', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+
+        // first get anf fill the email field selector
+        await page.locator('#user-name').fill(credentials[0].user_name);
+        // second get anf fill the email field selector
+        await page.locator('#password').fill(credentials[0].pass_word);
+        // now click to login/submit button
+        await page.locator('#login-button').click();
+
+        await page.waitForURL('https://www.saucedemo.com/inventory.html');
+
+        // Expecting the given URL
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+        await page.waitForTimeout(6000);
+    });
+});
